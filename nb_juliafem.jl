538a776e8abae82452afa5430d20327ccf16ffa2
amdeld <amdeld@gmail.com>
2021-01-03 15:24:20 +0100

    commit format

--- nb_juliafem.jl
+++ nb_juliafem.jl
@@ -4,6 +4,14 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 3f3ea480-4dc8-11eb-2ae0-effbf35a2819
+begin 
+	using BenchmarkTools
+	using DataFrames
+	using PrettyTables
+	using CSV
+end
+
 # ╔═╡ 425bfbf0-4c19-11eb-0a6e-6d5a00a82793
 md"# _Bienvenue dans Pluto!_
 
@@ -22,7 +30,7 @@ md"
 "
 
 # ╔═╡ fdd145a0-4c2a-11eb-14db-8b615050cc95
-3 + 4
+3 + 5
 
 # ╔═╡ c92a9d50-4c2b-11eb-373c-111930018fc3
 typeof(7) #nombre entier stoché en 64 bits en raison de l'architecture machine
@@ -60,16 +68,16 @@ md"
 "
 
 # ╔═╡ 3a714aa0-4c2b-11eb-22aa-cf44281380a7
-α = 3 # variable latex-unicode  names — type it by "\alpha<tab>"
+α = 6 # variable latex-unicode  names — tapez "\alpha<tab>"
 
 # ╔═╡ 626c1ba0-4c32-11eb-33da-3b2914157ab5
 typeof(α)
 
 # ╔═╡ 59033820-4c2b-11eb-33e1-75045e1bb19c
- y= 2α+1.5
+ β= 2α+1.5
 
 # ╔═╡ 71551810-4c32-11eb-23d1-07a71cc94923
-typeof(y)
+typeof(β)
 
 # ╔═╡ 23dd3ef0-4c32-11eb-057d-4be03341d81b
 md"
@@ -78,6 +86,34 @@ md"
 
 # ╔═╡ 77b027b0-4c2b-11eb-1c56-9d5a448c9b72
 x = [1, 17, 32, 15]  # les éléments sont séparés par des virgules
+
+# ╔═╡ 93258ec0-4c32-11eb-3d79-691f0f9b8a3d
+y = [15, 2, 6, -9]
+
+# ╔═╡ 7bb27c70-4c33-11eb-0237-510b388f0608
+x+y
+
+# ╔═╡ 05e1817e-4c33-11eb-3980-ab1373ea590f
+transpose(x)
+
+# ╔═╡ 45acaec0-4c33-11eb-2a47-8d2e97fdc8d5
+transpose(x)*y # produit scalaire
+
+# ╔═╡ 40f89c90-4c33-11eb-353a-8755d568e83a
+x .* y # produit élément par élément (broadcast)
+
+# ╔═╡ 3fe78420-4dc7-11eb-297d-1b95a81b9d5c
+pwd()
+
+# ╔═╡ ded07220-4dc9-11eb-1ff6-ff750d15705b
+
+
+# ╔═╡ 6cefc430-4dc9-11eb-2f08-b1ca17b64307
+@show P = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv",
+    "programminglanguages1.csv")
+
+# ╔═╡ c62fee40-4dc8-11eb-15a0-df65c45ebd16
+C = CSV.File(P)
 
 # ╔═╡ Cell order:
 # ╟─425bfbf0-4c19-11eb-0a6e-6d5a00a82793
@@ -100,3 +136,13 @@ x = [1, 17, 32, 15]  # les éléments sont séparés par des virgules
 # ╠═71551810-4c32-11eb-23d1-07a71cc94923
 # ╟─23dd3ef0-4c32-11eb-057d-4be03341d81b
 # ╠═77b027b0-4c2b-11eb-1c56-9d5a448c9b72
+# ╠═93258ec0-4c32-11eb-3d79-691f0f9b8a3d
+# ╠═7bb27c70-4c33-11eb-0237-510b388f0608
+# ╠═05e1817e-4c33-11eb-3980-ab1373ea590f
+# ╠═45acaec0-4c33-11eb-2a47-8d2e97fdc8d5
+# ╠═40f89c90-4c33-11eb-353a-8755d568e83a
+# ╠═3fe78420-4dc7-11eb-297d-1b95a81b9d5c
+# ╠═3f3ea480-4dc8-11eb-2ae0-effbf35a2819
+# ╠═ded07220-4dc9-11eb-1ff6-ff750d15705b
+# ╠═6cefc430-4dc9-11eb-2f08-b1ca17b64307
+# ╠═c62fee40-4dc8-11eb-15a0-df65c45ebd16
